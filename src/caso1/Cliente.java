222b38006d79074256f3fd311778acdbe286131d
DiegoLudena <d_l_m88@yahoo.es>
2024-02-14 13:18:35 +0100

    Version 1, la conexion se corta

--- src/caso1/Cliente.java
+++ src/caso1/Cliente.java
@@ -1,19 +1,64 @@
 package caso1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Cliente {
 	
 	private final int PUERTO = 1234;
 	private final String HOST = "localhost";
 	private Socket socketCliente;
+	private DataOutputStream flujoServidor;
+	private DataInputStream flujoCliente;
+
+	
+	Scanner scanner = new Scanner(System.in);
+
 	
 	public void iniciarCliente() {
 		try {
 			//Creo el socket e intento conectar con el servidor. Por si falla, hace falta rodearlo de try/catch
 			socketCliente = new Socket(HOST,PUERTO);
+			
+			//Hago que el usuario introduzca los números y la opción que quiere
+			 int num1 = obtenerNumero("Introduce el primer número:");
+	         int num2 = obtenerNumero("Introduce el segundo número:");
+			
+			System.out.println("Indica la operación a realizar: \n Suma\n Resta \n Multiplicacion  \n División");
+			String opcion = scanner.nextLine().toLowerCase();
+			
+			//envío al servidor 
+			//Flujo de datos hacia el servidor
+			flujoServidor = new DataOutputStream(socketCliente.getOutputStream());
+			
+			flujoServidor.writeInt(num1);
+			flujoServidor.writeInt(num2);
+			
+			flujoServidor.writeUTF(opcion);
+			
+
+			
+			//Creo el flujo de entrada para recibir 
+			flujoCliente = new DataInputStream(socketCliente.getInputStream());
+			String resultado = flujoCliente.readUTF();
+			
+			//Imprimo el resultado
+			System.out.println("El resultado de la " + opcion +" es: " + resultado);
+			
+			//Cierro los flujos
+			flujoServidor.close();
+			flujoCliente.close();
+			//Cierro el socket
+			System.out.println("Desconexión");
+			socketCliente.close();
+			
+			
+			
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -23,5 +68,13 @@ public class Cliente {
 		}
 
 	}
-
+	
+//Ya que hago varias veces el obtener un número, lo hago como un método
+    private int obtenerNumero(String mensaje) {
+        System.out.println(mensaje);
+        return scanner.nextInt();
+    }
 }
+
+
+
--- src/caso1/Servidor.java
+++ src/caso1/Servidor.java
@@ -1,5 +1,7 @@
 package caso1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -10,6 +12,11 @@ public class Servidor {
 	private final String HOST = "localhost";
 	private Socket socketCliente;
 	private ServerSocket socketServidor;
+	private DataInputStream flujoServidor;
+	private DataOutputStream flujoCliente;
+	
+
+
 	
 	
 	public void iniciarservidor() {
@@ -19,14 +26,60 @@ public class Servidor {
 			socketServidor = new ServerSocket(PUERTO);
 			socketCliente = new Socket();
 			
-			//Cuando llega una petición la acepta
+			//Cuando llega una petición la acepta. Pongo mensaje para comprobarlo.
 			socketCliente = socketServidor.accept();
+			System.out.println("Cliente conectado");
 			
+			//abro el flujo desde el cliente
+			flujoServidor = new DataInputStream(socketCliente.getInputStream());
+            // Leo los números y la opción enviados por el cliente
+            int num1 = flujoServidor.readInt();
+            int num2 = flujoServidor.readInt();
+            String opcion = flujoServidor.readUTF();
+			
+            //Hago el cálculo con los datos del usuario
+            String resultado = calculadora(num1, num2, opcion);
+            
+            //Creo el flujo de salida para enviar el mensaje
+            flujoCliente = new DataOutputStream(socketCliente.getOutputStream());
+            
+            //Envio el mensaje
+            flujoCliente.writeUTF(resultado);
+			
+            //Cierro los flujos
+            flujoServidor.close();
+            flujoCliente.close();
+            
+            //Cierro la conexión
+			System.out.println("Fin de la conexión.");
+			socketServidor.close();
+            
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		
 	}
+	
+	//Creo la calculadora. Inicialmente quería devolver un Int, pero no se devolver una cadena de error en el default, así que lo he convertido a String
+	public String calculadora(int num1, int num2, String opcion) { 
+	    switch(opcion) {
+	        case "suma":
+	            return String.valueOf(num1 + num2);
+	        case "resta":
+	            return String.valueOf(num1 - num2);
+	        case "multiplicacion":
+	            return String.valueOf(num1 * num2);
+	        case "division":
+	            // Evito el error en caso de que num 2 sea 0
+	            if (num2 != 0) {
+	                return String.valueOf(num1 / num2);
+	            } else {
+	                return "Error: División por cero";
+	            }
+	        default:
+	            return "Opción inválida";
+	    	}
+		}
 
 }
